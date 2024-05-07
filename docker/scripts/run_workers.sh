ef04db9524742cb22379ff06b73f333c876413bc
harsh <66548698+harsssh@users.noreply.github.com>
2024-05-08 04:10:54 +0900

    update worker script

--- docker/scripts/run_workers.sh
+++ docker/scripts/run_workers.sh
@@ -7,6 +7,7 @@
 : "${FSCHAT_WORKER_PORT:?}"
 : "${FSCHAT_CONTROLLER_PORT:?}"
 : "${FSCHAT_WORKER_NUM_GPUS:?}"
+: "${FSCHAT_WORKER_CONV_TEMPLATE:?}"
 
 # FSCHAT_WORKER_MODEL_PATHS はカンマ区切りのリスト
 # --model-path value1 --model-path value2 ... という形に変換する
@@ -22,4 +23,5 @@ python3.9 -m fastchat.serve.multi_model_worker \
   --model-names "$FSCHAT_WORKER_MODEL_NAMES" \
   --worker-address "http://fastchat-model-worker:$FSCHAT_WORKER_PORT" \
   --controller-address "http://fastchat-controller:$FSCHAT_CONTROLLER_PORT" \
-  --num-gpus "$FSCHAT_WORKER_NUM_GPUS"
+  --num-gpus "$FSCHAT_WORKER_NUM_GPUS" \
+  --conv-template "$FSCHAT_WORKER_CONV_TEMPLATE"
