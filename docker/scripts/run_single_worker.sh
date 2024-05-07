a81bffa8b8e39bc2c62059d9486c618a6bc21bd5
harsh <66548698+harsssh@users.noreply.github.com>
2024-05-08 02:08:00 +0900

    fix script

--- docker/scripts/run_single_worker.sh
+++ docker/scripts/run_single_worker.sh
@@ -8,18 +8,15 @@
 : "${FSCHAT_CONTROLLER_PORT:?}"
 : "${FSCHAT_WORKER_NUM_GPUS:?}"
 
-model_path_args=()
-for model_path in $(echo "$FSCHAT_WORKER_MODEL_PATHS" | tr ',' ' '); do
-  model_path_args+=(--model-path "$model_path")
-  break # 1つ目モデルだけ使う
-done
+# 最初のモデルのみを使う
+# カンマ区切りの最初だけ
+model_args="--model-path $(echo "$FSCHAT_WORKER_MODEL_PATHS" | cut -d, -f1) --model-name $(echo "$FSCHAT_WORKER_MODEL_NAMES" | cut -d, -f1)"
 
 python3.9 -m fastchat.serve.model_worker \
   --host "$FSCHAT_WORKER_HOST" \
   --port "$FSCHAT_WORKER_PORT" \
-  "${model_path_args[@]}" \
-  --model-names "$FSCHAT_WORKER_MODEL_NAMES" \
+  "$model_args" \
   --worker-address "http://fastchat-model-worker:$FSCHAT_WORKER_PORT" \
   --controller-address "http://fastchat-controller:$FSCHAT_CONTROLLER_PORT" \
   --num-gpus "$FSCHAT_WORKER_NUM_GPUS" \
-  --debug
+  --debug DEBUG
